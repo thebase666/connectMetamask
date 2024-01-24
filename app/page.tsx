be252bcbb3a607c952c413e46593a720223bb5f8
LeoTheBase <lizhuyao666@gmail.com>
2024-01-24 18:42:37 +0800

    1

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div className=' text-white h-screen snap-y snap-mandatory overflow-x-hidden overflow-y-hidden z-0 '>
+    <div className='  text-white h-screen snap-y snap-mandatory overflow-x-hidden overflow-y-hidden z-0 '>
       <Hero />
     </div>
   );
